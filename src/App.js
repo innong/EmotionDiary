b8c87a18a5ebe08baa375cc6a5bb67754746a8ff
innong <inyoung424@gmail.com>
2022-04-20 16:35:53 +0900

    [FEAT] 페이지 라우팅 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes , Route} from 'react-router-dom';
 import './App.css';
+
+import Home from './pages/Home';
+import Diary from './pages/Diary';
+import New from './pages/New';
+import Edit from './pages/Edit';
+
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <h2>app.js</h2>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/edit" element={<Edit />} />
+          <Route path="/diary" element={<Diary />} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div>
+            <h1>Home</h1>
+            <p>이 곳은 일기 홈 페이지 입니다.</p>
+        </div>
+    );
+};
+
+export default Home;
