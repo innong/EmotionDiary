d25a74761aeaa1a0d1e48c5a5bf63ad56bb0b1da
innong <inyoung424@gmail.com>
2022-04-21 22:17:55 +0900

    [FEAT]페이지 라우팅-useNavigate

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,8 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const Edit = () => {
-
-    const [searchParams,setSearchParms] = useSearchParams();
+    const navigate = useNavigate();
+    const [searchParams, setSearchParms] = useSearchParams();
 
     const id = searchParams.get('id');
     console.log("id : ", id);
@@ -14,7 +14,21 @@ const Edit = () => {
         <div>
             <h1>Edit</h1>
             <p>이 곳은 일기 수정 페이지 입니다.</p>
-            <button onClick={() => setSearchParms({who: 'inyoung'})}>QS 바꾸기</button>
+            <button onClick={() => setSearchParms({ who: 'inyoung' })}>
+                QS 바꾸기
+            </button>
+
+            <button onClick={() => {
+                navigate("/home");
+            }}>
+                HOME으로 가기
+            </button>
+
+            <button onClick={() => {
+                navigate(-1);
+            }}>
+                뒤로 가기
+            </button>
         </div>
     );
 };
