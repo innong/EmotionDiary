cc03163a078a898d48ff2a4395fdaeb43ac228de
innong <inyoung424@gmail.com>
2022-04-22 17:32:06 +0900

    [FEAT]Home-list 정렬 구현

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,17 +1,60 @@
+// diary list 정렬 기능 위해 component 만들기
+import { useState } from "react";
+
+const sortOptionList = [
+  { value: "latest", name: "최신순" },
+  { value: "oldest", name: "오래된 순" },
+];
+
+const ControlMenu = ({ value, onChange, optionList }) => {
+  return (
+    <select value={value} onChange={(e) => onChange(e.target.value)}>
+      {optionList.map((it, idx) => (
+        <option key={idx} value={it.value}>
+          {it.name}
+        </option>
+      ))}
+    </select>
+  );
+};
+
 const DiaryList = ({ diaryList }) => {
-    return (
-        <div>
-            {diaryList.map((it) => (
-                <div key={it.id}>
-                    {it.content}
-                </div>
-            ))}
+  const [sortType, setSortType] = useState("latest");
+
+  const getProcessedDiaryList = () => {
+    const compare = (a, b) => {
+      if (sortType === 'latest') {
+        return parseInt(b.date) - parseInt(a.date);
+      } else {
+        return parseInt(a.date) - parseInt(b.date);
+      }
+    }
+
+    const copyList = JSON.parse(JSON.stringify(diaryList));
+    const sortedList = copyList.sort(compare);
+
+    return sortedList;
+  };
+
+  return (
+    <div>
+      <ControlMenu
+        value={sortType}
+        onChange={setSortType}
+        optionList={sortOptionList}
+      />
+
+      {getProcessedDiaryList().map((it) => (
+        <div key={it.id}>
+          {it.content}
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 DiaryList.defaultProps = {
-    diaryList : [],
+  diaryList: [],
 };
 
 export default DiaryList;
