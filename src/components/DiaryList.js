3190a3c5149c53697b1bec9ef7d7ae98ee5cb531
innong <inyoung424@gmail.com>
2022-04-22 17:53:43 +0900

    [FEAT]Home-감정 필터 구현

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -6,6 +6,12 @@ const sortOptionList = [
   { value: "oldest", name: "오래된 순" },
 ];
 
+const filterOptionList = [
+  { value: "all", name: "전부다" },
+  { value: "good", name: "좋은 감정만" },
+  { value: "bad", name: "안 좋은 감정만" },
+]
+
 const ControlMenu = ({ value, onChange, optionList }) => {
   return (
     <select value={value} onChange={(e) => onChange(e.target.value)}>
@@ -20,18 +26,30 @@ const ControlMenu = ({ value, onChange, optionList }) => {
 
 const DiaryList = ({ diaryList }) => {
   const [sortType, setSortType] = useState("latest");
+  const [filter, setFilter] = useState("all");
 
   const getProcessedDiaryList = () => {
+    const filterCallback = (item) => {
+      if (filter === 'good') {
+        return parseInt(item.emotion) <= 3;
+      } else {
+        return parseInt(item.emotion) > 3;
+      }
+    };
+
     const compare = (a, b) => {
       if (sortType === 'latest') {
         return parseInt(b.date) - parseInt(a.date);
       } else {
         return parseInt(a.date) - parseInt(b.date);
       }
-    }
+    };
 
     const copyList = JSON.parse(JSON.stringify(diaryList));
-    const sortedList = copyList.sort(compare);
+
+    const filteredList =
+      filter === "all" ? copyList : copyList.filter((it) => filterCallback(it));
+    const sortedList = filteredList.sort(compare);
 
     return sortedList;
   };
@@ -43,10 +61,14 @@ const DiaryList = ({ diaryList }) => {
         onChange={setSortType}
         optionList={sortOptionList}
       />
-
+      <ControlMenu
+        value={filter}
+        onChange={setFilter}
+        optionList={filterOptionList}
+      />
       {getProcessedDiaryList().map((it) => (
         <div key={it.id}>
-          {it.content}
+          {it.content} {it.emotion}
         </div>
       ))}
     </div>
