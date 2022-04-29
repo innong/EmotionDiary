345b7a4b892d44de7009ae6ac9fbdbbb981508c9
innong <inyoung424@gmail.com>
2022-04-29 12:47:13 +0900

    Feat:New page,add 'emotion_list_wrapper'

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -3,6 +3,35 @@ import { useNavigate } from "react-router-dom";
 
 import MyHeader from "./MyHeader";
 import MyButton from "./MyButton";
+import EmotionItem from "./EmotionItem";
+
+const emotionList = [
+    {
+        emotion_id: 1,
+        emotion_img: process.env.PUBLIC_URL + `/assets/emotion1.png`,
+        emotion_descript: '완전 좋음'
+    },
+    {
+        emotion_id: 2,
+        emotion_img: process.env.PUBLIC_URL + `/assets/emotion2.png`,
+        emotion_descript: '좋음'
+    },
+    {
+        emotion_id: 3,
+        emotion_img: process.env.PUBLIC_URL + `/assets/emotion3.png`,
+        emotion_descript: '보통'
+    },
+    {
+        emotion_id: 4,
+        emotion_img: process.env.PUBLIC_URL + `/assets/emotion4.png`,
+        emotion_descript: '나쁨'
+    },
+    {
+        emotion_id: 5,
+        emotion_img: process.env.PUBLIC_URL + `/assets/emotion5.png`,
+        emotion_descript: '완전 나쁨'
+    }
+];
 
 const getStringDate = (date) => {
     return date.toISOString().slice(0, 10);
@@ -11,10 +40,15 @@ const getStringDate = (date) => {
 const DiaryEditor = () => {
     const [date, setDate] = useState(getStringDate(new Date()));
 
+    const [emotion, setEmotion] = useState(3);
+    const handleClickEmote = (emotion) => {
+        setEmotion(emotion);
+    };
+
     const navigate = useNavigate();
 
     return (
-        <div>
+        <div className="DiaryEditor">
             <MyHeader
                 headText={"새 일기 쓰기"}
                 leftChild={<MyButton text={"뒤로 가기"} onClick={() => navigate(-1)} />}
@@ -22,13 +56,26 @@ const DiaryEditor = () => {
 
             <div>
                 <section>
-                    <h2>오늘은 언제인가요?</h2>
-                    <div className="input-box">
+                    <h4>오늘은 언제인가요?</h4>
+                    <div className="input_box">
                         <input
-                            className="input-date"
+                            className="input_date"
                             value={date}
                             onChange={(e) => setDate(e.target.value)}
                             type="date" />
+                    </div>
+                </section>
+
+                <section>
+                    <h4>오늘의 감정</h4>
+                    <div className="input_box emotion_list_wrapper">
+                        {emotionList.map((it) => (
+                            <EmotionItem
+                                key={it.emotion_id} {...it}
+                                onClick={handleClickEmote}
+                                isSelected={it.emotion_id === emotion}
+                            />
+                        ))}
                     </div>
                 </section>
             </div>
